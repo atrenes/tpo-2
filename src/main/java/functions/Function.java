663d06d0b14c90111ed1800d290a9ae43b21f2fd
atrenes <hanalainendm@gmail.com>
2023-03-09 21:22:36 +0300

    function abstract class

--- src/main/java/functions/Function.java
+++ src/main/java/functions/Function.java
@@ -1,4 +1,13 @@
 package functions;
 
-public class Function {
+public abstract class Function {
+    private final double accuracy;
+
+    public Function(double accuracy) {
+        this.accuracy = accuracy;
+    }
+
+    public double getAccuracy() {return this.accuracy;}
+
+    public abstract double compute(double x);
 }
